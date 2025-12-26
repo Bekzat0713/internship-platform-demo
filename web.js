1bf204ea450ace4f9d09f57d77fa92df1dfe5b28
Bekzat0713 <bmuftahiden@gmail.com>
2025-12-26 20:47:04 +0800

    Day 2 main page

---
+++ web.js
@@ -0,0 +1,7 @@
+document.getElementById("studentBtn").onclick = function () {
+  alert("Раздел для студентов скоро будет доступен");
+};
+
+document.getElementById("companyBtn").onclick = function () {
+  alert("Раздел для работодателей скоро будет доступен");
+};
